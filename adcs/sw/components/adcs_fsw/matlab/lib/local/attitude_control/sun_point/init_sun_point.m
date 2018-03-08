a11f1d3f2de7ed7b82328e24bb9c4539ac92d060
U-KRISH-XPS\Krish <krish_kaycee@hotmail.com>
2018-03-07 22:21:39 -0800

    initial commit of sun-point controller

---
+++ adcs/sw/components/adcs_fsw/matlab/lib/local/attitude_control/sun_point/init_sun_point.m
@@ -0,0 +1,9 @@
+function sun_point = init_sun_point
+
+sun_point.ic.meas_ss_valid = 0;
+sun_point.ic.meas_ss_body_unit = [0 0 0];
+sun_point.ic.meas_pd_body_unit = [0 0 0];
+sun_point.ic.meas_body_rate_radps = [0 0 0];
+sun_point.ic.error_vec = [0 0 0];
+
+end
